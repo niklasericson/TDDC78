fb50d0599f090df265691234d6921c04be2e42f2
patny205 <patny205@student.liu.se>
2015-04-16 16:05:03 +0200

    Now able to scatter and gather an image by rows to aribtrariy number of cores.

--- Lab1/blurmain.c
+++ Lab1/blurmain.c
@@ -12,41 +12,54 @@
     #include <mach/mach.h>
 #endif
 
+#define MAX_RAD 1000
 
 int main (int argc, char ** argv) {
    int radius;
     int xsize, ysize, colmax;
     pixel src[MAX_PIXELS];
     struct timespec stime, etime;
-#define MAX_RAD 1000
 
-    MPI_Comm com = MPI_COMM_WORLD;
+
+    MPI_Comm com, scatter_com;
+    com = MPI_COMM_WORLD;
     MPI_Init(&argc, &argv);
 
     int myid, np, lsize;
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(com, &np);
 
+    printf("np: %d \n", np);
+
     MPI_Status status;
+
+    int color;
+    int key;
+
+    color = (myid == (np - 1));
+    key = myid;
+
+    MPI_Comm_split(com, color, key, &scatter_com);
 
     double w[MAX_RAD];
 
     /* Take care of the arguments */
 
     if (argc != 4) {
-    fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
-    exit(1);
+        fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
+        exit(1);
     }
     radius = atoi(argv[1]);
+    
     if((radius > MAX_RAD) || (radius < 1)) {
-    fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
-    exit(1);
+        fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
+        exit(1);
     }
 
     if (myid == 0) {
     /* read file */
         if(read_ppm (argv[2], &xsize, &ysize, &colmax, (char *) src) != 0) {
-             exit(1);    
+             exit(2);
         }
 
         if (colmax > 255) {
@@ -60,10 +73,9 @@ int main (int argc, char ** argv) {
 
     /* filter */
 
-
-    //printf("Calling filter\n");
-
-    //clock_gettime(CLOCK_REALTIME, &stime);
+    if (myid == 0) {
+        printf("Calling filter\n");
+    }
 
     #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
         clock_serv_t cclock;
@@ -87,72 +99,103 @@ int main (int argc, char ** argv) {
     xsize = size[0];
     ysize = size[1];
 
-    // lsize = Number of pixels for every process.
-    int rows_per_core = ysize/(np - 1);
+    // lsize = Number of rows for every process execpt np - 1, wich will 
+    // contain the remaining rows (since ysize/(np - 1) might not be whole number.
+    int rows_per_core = ysize/np;
+
+    // Reamining rows for last core to process
+    int remaining_rows = ysize % np + rows_per_core;
+
+    printf("ysize: %d, rows_per_core: %d, remaining_rows: %d \n", ysize, rows_per_core, remaining_rows);
 
     lsize = rows_per_core*xsize;
+    int lsize_last_core = remaining_rows*xsize;
+
+    register int last_start_pos = lsize*(np - 1);
 
     printf("lsize: %d\n", lsize);
     printf("Number of pixels: %d\n", xsize*ysize);
 
     pixel* local;
 
-    local = (pixel *)malloc((lsize + 2*radius)*sizeof(pixel));
+    local = (pixel *)malloc((lsize_last_core + 2*radius)*sizeof(pixel));
 
-
-    // local + radius
     MPI_Scatter(src, lsize*sizeof(pixel), MPI_CHAR, local,
-                lsize*sizeof(pixel), MPI_CHAR,0,com);
+                lsize*sizeof(pixel), MPI_CHAR,0,scatter_com);
+
+    if (myid == 0) {
+        MPI_Send(src + last_start_pos, lsize_last_core*sizeof(pixel),
+                    MPI_CHAR, np-1, 10, com);
+    }
+    else if (myid == (np - 1)) {
+        MPI_Recv(local, lsize_last_core*sizeof(pixel), 
+                    MPI_CHAR, 0, 10, com, &status);
+    }
 
     printf("ID: %d\n",myid);
 
+    
     if (myid == 0) {
-        if (write_ppm ("im1procces0.ppm", xsize, ysize/np, (char *) local) != 0) {
+        if (write_ppm ("im1procces0.ppm", xsize, rows_per_core, (char *) local) != 0) {
             printf("Write failed in scatter check\n");
         }
     }
     else if (myid == 1) {
-        if (write_ppm ("im1procces1.ppm", xsize, ysize/np, (char *) local) != 0) {
+        if (write_ppm ("im1procces1.ppm", xsize, rows_per_core, (char *) local) != 0) {
             printf("Write failed in scatter check\n");
         }
     }
     else if (myid == 2) {
-        if (write_ppm ("im1procces2.ppm", xsize, ysize/np, (char *) local) != 0) {
+        if (write_ppm ("im1procces2.ppm", xsize, rows_per_core, (char *) local) != 0) {
             printf("Write failed in scatter check\n");
         }
     }
     else if (myid == 3) {
-        if (write_ppm ("im1procces3.ppm", xsize, ysize/np, (char *) local) != 0) {
+        if (write_ppm ("im1procces3.ppm", xsize, remaining_rows, (char *) local) != 0) {
             printf("Write failed in scatter check\n");
         }
     }
 
+
     //splitblur(xsize, ysize, src, radius, w);
 
-    //clock_gettime(CLOCK_REALTIME, &etime);
+
+    MPI_Gather(local, lsize*sizeof(pixel), MPI_CHAR, src, lsize*sizeof(pixel),
+                MPI_CHAR, 0, scatter_com);
+
+    if (myid == 0) {
+        MPI_Recv(src + last_start_pos, lsize_last_core*sizeof(pixel), 
+                    MPI_CHAR, np-1, 20, com, &status);
+    }
+    else if (myid == np-1) {
+        MPI_Send(local, lsize_last_core*sizeof(pixel), 
+                    MPI_CHAR, 0, 20, com);
+    }
+
 
     #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-        //clock_serv_t cclock;
-        //mach_timespec_t mts;
         host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
         clock_get_time(cclock, &mts);
         mach_port_deallocate(mach_task_self(), cclock);
         etime.tv_sec = mts.tv_sec;
         etime.tv_nsec = mts.tv_nsec;
-
     #else
         clock_gettime(CLOCK_REALTIME, &etime);
     #endif
 
-    printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
 
     /* write result */
-    printf("Writing output file\n");
     
-    if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0) {
-      printf("Write failed writing src\n");
-      exit(1);
+    if (myid == 0) {
+        printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
+	           1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+        
+        printf("Writing output file\n");
+        
+        if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0) {
+            printf("Write failed writing src\n");
+            exit(1);
+        }
     }
 
     MPI_Finalize();
